b9a64623a291aecf758d7d642df6bc75120d27c1
aprilespiritu <april_espiritu@live.com>
2024-05-23 22:43:15 +0800

    Gallery update

---
+++ src/js/movie-gallery.js
@@ -0,0 +1,33 @@
+import { API_KEY } from "./constants/moviedb";
+
+const apiUrl = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`;
+const moviesContainer = document.getElementById("movies");
+
+async function fetchMovies() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        data.results.forEach(media => {
+            const movieCard = createMovieCard(media);
+            moviesContainer.appendChild(movieCard);
+        });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+function createMovieCard(media) {
+    const { title, name, backdrop_path } = media;
+
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie_item")
+
+    movieCard.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" class="movie_img_rounded">
+        <div class = "title">${title || name}</div>
+    `;
+    return movieCard;
+}
+
+fetchMovies();
