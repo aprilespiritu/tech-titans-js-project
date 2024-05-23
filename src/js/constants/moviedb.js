a96ab80118b0884e6582042556c4ae445f5d2962
aprilespiritu <april_espiritu@live.com>
2024-05-23 16:15:28 +0800

    Gallery update

--- src/js/constants/moviedb.js
+++ src/js/constants/moviedb.js
@@ -1,2 +1,22 @@
-export const API_KEY = 'ea088de809b9eab9d622eab39f8395fb';
+export const API_KEY = 'add7393009d30185561651a8e7e3f1c4';
 export const BASE_URL = 'https://api.themoviedb.org/3/';
+
+import axios from 'axios';
+
+const options = {
+  method: 'GET',
+  url: 'https://api.themoviedb.org/3/movie/now_playing',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZGQ3MzkzMDA5ZDMwMTg1NTYxNjUxYThlN2UzZjFjNCIsInN1YiI6IjY2NGFmMWFmNmY0MGMzOTVmNDkzMzJkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.st8Oc5Zd3hxX4whgU159arSQTJBdBGx9DbocLAqn8Nk'
+  }
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
